7dc3fcbab77cc7d35a2f9ecd11cbd153f0d74e37
MedSkander <92828246+MedSkanderRomdhani@users.noreply.github.com>
2023-11-11 23:01:58 +0100

    dockerfile

---
+++ src/app/shared/Service/Secteur-activite.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecteurActiviteService {
+  readonly API_URL = environment.API_Base;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllSecteurActivites() {
+    return this.httpClient.get(`${this.API_URL}/secteur-activite/retrieve-all-secteurActivite`)
+  }
+  addSecteurActivite(secteurActivite : any) {
+    return this.httpClient.post(`${this.API_URL}/secteur-activite/add-secteurActivite`, secteurActivite)
+  }
+  editSecteurActivite(secteurActivite : any){
+    return this.httpClient.put(`${this.API_URL}/secteur-activite/modify-secteurActivite`, secteurActivite)
+  }
+  deleteSecteurActivite(idSecteurActivite : any){
+    return  this.httpClient.delete(`${this.API_URL}/secteur-activite/remove-secteurActivite/${idSecteurActivite}`)
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  API_Base :'http://localhost:8082/SpringMVC/'
+};
